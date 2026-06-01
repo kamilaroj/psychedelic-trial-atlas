adb27a447d6b126d5859d1993cef14571a9ac8c0
kamilaroj <kamila.rojek@gmail.com>
2026-06-01 16:53:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-<iframe
-  width="100%"
-  height="900"
-  frameBorder="0"
-  src="https://observablehq.com/embed/e3028f2577c04f9a?cells=visual3Chart"
-></iframe>
+export default function App() {
+  return (
+    <iframe
+      width="100%"
+      height="900"
+      frameBorder="0"
+      src="https://observablehq.com/embed/e3028f2577c04f9a?cells=visual3Chart"
+    />
+  );
+}
