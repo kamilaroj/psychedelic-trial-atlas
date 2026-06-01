1602a5a96166895b81e37aa1e673e4cd4a3a7c91
kamilaroj <kamila.rojek@gmail.com>
2026-06-01 16:52:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,6 @@
-<iframe width="100%" height="853" frameborder="0"
-  src="https://observablehq.com/embed/e3028f2577c04f9a@666?cells=visual3Chart"></iframe>
+<iframe
+  width="100%"
+  height="900"
+  frameBorder="0"
+  src="https://observablehq.com/embed/e3028f2577c04f9a?cells=visual3Chart"
+></iframe>
