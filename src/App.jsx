b005d55c525c6ebc5be28c0587d2545617c4555f
kamilaroj <kamila.rojek@gmail.com>
2026-06-09 12:56:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,23 @@ function App() {
       }}
     >
       <iframe
+        title="Hero Section"
+        width="100%"
+        height="836"
+        frameBorder="0"
+        src="https://observablehq.com/embed/e3028f2577c04f9a@870?cells=heroSection&api_key=56b35abcad40d7e4fbaa74604e78458cb605565c"
+        style={{
+          display: "block",
+          width: "100%",
+          height: "836px",
+          border: "0",
+          margin: "0",
+          padding: "0",
+          background: "#f1f0ec",
+        }}
+      />
+
+      <iframe
         title="Visual 1 Company Landscape"
         width="100%"
         height="405"
