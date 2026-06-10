dc0bbb46ac70dd246b6f5f4100049f5d59567ab3
kamilaroj <kamila.rojek@gmail.com>
2026-06-10 10:15:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,48 @@ function App() {
       style={{
         background: "#f1f0ec",
         minHeight: "100vh",
-        margin: 0,
-        padding: 0,
-        fontFamily: "Inter, Arial, sans-serif",
+        color: "#1d1d1f",
+        fontFamily:
+          '"Inter", "Helvetica Neue", Arial, system-ui, sans-serif',
       }}
     >
-      <iframe
-        title="Psychedelic Trial Atlas"
-        width="100%"
-        height="1978"
-        frameBorder="0"
-        src="https://observablehq.com/embed/e3028f2577c04f9a@870?cells=heroSection%2Cvisual1EcosystemOverview%2Cvisual1CompanyLandscape&api_key=56b35abcad40d7e4fbaa74604e78458cb605565c"
+      <section
         style={{
-          display: "block",
           width: "100%",
-          height: "1978px",
-          border: 0,
-          margin: 0,
-          padding: 0,
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          padding: "0",
+          margin: "0",
           background: "#f1f0ec",
         }}
-      />
+      >
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "1600px",
+            margin: "0 auto",
+            padding: "0",
+          }}
+        >
+          <iframe
+            title="UNICORN1 Psychedelic Trial Atlas Visual 1"
+            width="100%"
+            height="2245.890625"
+            frameBorder="0"
+            src="https://observablehq.com/embed/e3028f2577c04f9a@888?cells=heroSection%2Cvisual1EcosystemOverview%2CcreateVisual1CompanyLandscape%2Cvisual1CompanyLandscapePremium&api_key=4435d20fe2d84784a31c5b7c2aa450150c9773a7"
+            style={{
+              display: "block",
+              width: "100%",
+              height: "2245.890625px",
+              border: "0",
+              margin: "0 auto",
+              background: "#f1f0ec",
+            }}
+          />
+        </div>
+      </section>
     </main>
   );
 }
