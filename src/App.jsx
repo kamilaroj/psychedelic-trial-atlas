9513558a9ec7e7aac9e1961d703f51d4ed000704
kamilaroj <kamila.rojek@gmail.com>
2026-06-24 13:03:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -897,8 +897,8 @@ export default function App() {
         heroIframe: 930,
         ecosystemVisible: 796.34375,
         ecosystemIframe: 796.34375,
-        companyVisible: 776.28125,
-        companyIframe: 776.28125,
+        companyVisible: 776.078125,
+        companyIframe: 776.078125,
         compoundVisible: 820,
         compoundIframe: 960,
         indicationVisible: 800,
@@ -910,7 +910,7 @@ export default function App() {
 
     const heroVisible = clampNumber(viewportHeight * 0.76, 600, 740);
     const ecosystemVisible = 796.34375;
-    const companyVisible = 776.28125;
+    const companyVisible = 776.078125;
     const compoundVisible = clampNumber(viewportHeight * 0.8, 660, 820);
     const indicationVisible = clampNumber(viewportHeight * 0.8, 660, 800);
     const phaseVisible = clampNumber(viewportHeight * 0.8, 660, 800);
@@ -923,7 +923,7 @@ export default function App() {
       ecosystemIframe: 796.34375,
 
       companyVisible,
-      companyIframe: 776.28125,
+      companyIframe: 776.078125,
 
       compoundVisible,
       compoundIframe: compoundVisible + 120,
@@ -994,7 +994,7 @@ export default function App() {
 
       visual1EcosystemOverviev: `https://observablehq.com/embed/e3028f2577c04f9a@1223?cells=visual1EcosystemOverviev&api_key=d7fef968784b35d048ae7ced9ccc3258c6778b29`,
 
-      visual1CompanyLandscapePremium1: `https://observablehq.com/embed/e3028f2577c04f9a@1221?cells=visual1CompanyLandscapePremium1&api_key=d7fef968784b35d048ae7ced9ccc3258c6778b29`,
+      visual1CompanyLandscapePremium1: `https://observablehq.com/embed/e3028f2577c04f9a@1224?cells=visual1CompanyLandscapePremium1&api_key=d7fef968784b35d048ae7ced9ccc3258c6778b29`,
 
       visual2Chartminimalistic1a: `https://observablehq.com/embed/e3028f2577c04f9a@1216?cells=visual2Chartminimalistic1&api_key=515f6c33729f1bf487d1dbfd16abac4e81acfbd2`,
 
