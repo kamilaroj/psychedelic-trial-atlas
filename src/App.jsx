058b0995cc5e9ed9165e091dd8674ba11b4c55d2
kamilaroj <kamila.rojek@gmail.com>
2026-09-01 11:13:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -584,12 +584,12 @@ export default function App() {
         `${heroResponsiveParams}`,
 
       visual1Overview:
-        `https://observablehq.com/embed/e3028f2577c04f9a@1448` +
+        `https://observablehq.com/embed/e3028f2577c04f9a@1450` +
         `?cells=visual1EcosystemOverviev` +
         `&api_key=ecf9f0bfb7b84e805b81fe519905418231789a18`,
 
       visual1Company:
-        `https://observablehq.com/embed/e3028f2577c04f9a@1448` +
+        `https://observablehq.com/embed/e3028f2577c04f9a@1450` +
         `?cells=visual1CompanyLandscapePremium1` +
         `&api_key=ecf9f0bfb7b84e805b81fe519905418231789a18` +
         `${visual1CompanyLandscapeParams}`,
