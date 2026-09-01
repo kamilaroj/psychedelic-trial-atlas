a5669d2a760d9d763e31b25e924213eae7f5f3bd
kamilaroj <kamila.rojek@gmail.com>
2026-09-01 14:55:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -546,18 +546,18 @@ export default function App() {
         `${heroResponsiveParams}`,
 
       visual1Overview:
-        `https://observablehq.com/embed/e3028f2577c04f9a@1486` +
+        `https://observablehq.com/embed/e3028f2577c04f9a@1487` +
         `?cells=visual1EcosystemOverviev` +
         `&api_key=ecf9f0bfb7b84e805b81fe519905418231789a18`,
 
       visual1Company:
-        `https://observablehq.com/embed/e3028f2577c04f9a@1486` +
+        `https://observablehq.com/embed/e3028f2577c04f9a@1487` +
         `?cells=visual1CompanyLandscapePremium1` +
         `&api_key=ecf9f0bfb7b84e805b81fe519905418231789a18` +
         `${visual1CompanyLandscapeParams}`,
 
       visual2:
-        `https://observablehq.com/embed/e3028f2577c04f9a@1486` +
+        `https://observablehq.com/embed/e3028f2577c04f9a@1487` +
         `?cells=visual2ChartUnitColumns1` +
         `&api_key=2488895c619fa293677a0791309b410e6db31cb6`,
 
@@ -1139,7 +1139,7 @@ export default function App() {
               title="Compound Activity Landscape"
               src={observableSrc.visual2}
               className="full-visual standard-analysis-visual full-compound-visual"
-              height={760}
+              height={790}
             />
           </section>
         )}
