af12b38f5efc2010a7f3c16d36c8fbf90c7ba18e
kamilaroj <kamila.rojek@gmail.com>
2026-09-02 13:31:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1061,7 +1061,7 @@ export default function App() {
         `?cells=visual2ChartUnitColumns1`,
 
       visual3:
-        "https://observablehq.com/embed/e3028f2577c04f9a@1508?cells=visual3Chart",
+        "https://observablehq.com/embed/e3028f2577c04f9a@1509?cells=visual3Chart",
 
       visual4:
         `https://observablehq.com/embed/e3028f2577c04f9a@1485` +
